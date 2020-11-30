76db706f655004a6030377b803d8e62c27067035
da5407 <da5407@gmail.com>
2020-11-30 13:39:42 +0800

    Download table

--- src/app/mblog/blog-list/blog-list.component.ts
+++ src/app/mblog/blog-list/blog-list.component.ts
@@ -73,4 +73,21 @@ export class BlogListComponent implements OnInit {
     this.router.navigateByUrl(`/blog/bdetail/${id}`);
   }
 
+downloadFile(data: Blog[], filename='data') {
+  let csvData = this.blogService.ConvertToCSV(data,Object.keys(this.blog));
+  console.log(csvData)
+        let blob = new Blob(['\ufeff' + csvData], { type: 'text/csv;charset=utf-8;' });
+        let dwldLink = document.createElement("a");
+        let url = URL.createObjectURL(blob);
+        let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+        if (isSafariBrowser) {  //if Safari open in new window to save file with random filename.
+            dwldLink.setAttribute("target", "_blank");
+        }
+        dwldLink.setAttribute("href", url);
+        dwldLink.setAttribute("download", filename + ".csv");
+        dwldLink.style.visibility = "hidden";
+        document.body.appendChild(dwldLink);
+        dwldLink.click();
+        document.body.removeChild(dwldLink);
+}  
 }
--- src/app/mblog/blog.service.ts
+++ src/app/mblog/blog.service.ts
@@ -58,4 +58,24 @@ export class BlogService {
       .catch(this.handleError);
   }
 
+  ConvertToCSV(objArray:any, headerList:any) {
+    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    let str = '';
+    let row = '';
+    for (let index in headerList) {
+     row += headerList[index] + ',';
+    }
+    row = row.slice(0, -1);
+    str += row + '\r\n';
+    for (let i = 0; i < array.length; i++) {
+     let line = '';
+     for (let index in headerList) {
+      let head = headerList[index];
+      line +=  array[i][head]+',' ;
+     }
+     str += line.substr(0, line.length -1) + '\r\n';
+    }
+    return str;
+   }
+  
 }
